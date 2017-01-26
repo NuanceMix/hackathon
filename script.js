a040e7a49b969179a31196c4ded2fa6c98b82b47
William Mura <william.mura@nuance.com>
2017-01-26 17:09:20 -0500

    Set url for embedded view

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var iframe = document.createElement("iframe");
-iframe.src = "http://199.245.57.4/";
+iframe.src = "http://199.245.57.4/embedded";
 
 $(document).ready(function() {
     $('#slack-invitation-iframe').append(iframe);
